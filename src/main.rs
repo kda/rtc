e0e6867d18e1502b0c16ee531cd9ea97e31899ea
Kevin Athey <kda@pobox.com>
2026-08-12 09:02:19 -0700

    add ratatui hello

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use ratatui::{DefaultTerminal, Frame};
+
+fn app(terminal: &mut DefaultTerminal) {
+    loop {
+        terminal.draw(render).expect("unable to draw terminal");
+        if crossterm::event::read().expect("failed to read keyboard event").is_key_press() {
+            break;
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
+
 fn main() {
-    println!("Hello, world!");
+    ratatui::run(app);
 }
